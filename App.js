f06382f39f4462cde04ed0ac4280beddb04375aa
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-11-16 13:25:52 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {View} from 'react-native';
+
+const App: () => Node = () => {
+
+  return (
+      <View>
+      </View>
+  );
+};
+
+export default App;
