48b0f54ed6c531a826d7c6bb913cb4e679f536c3
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-11-27 20:26:58 +0530

    created signInActionType

---
+++ screens/root-stack-screen/root-stacks/sign-in-screen/redux/signInActionType.js
@@ -0,0 +1,6 @@
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_IN_WITH_GOOGLE = 'SIGN_IN_WITH_GOOGLE';
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
+export const SIGN_IN_FAILURE = 'SIGN_IN_FAILURE';
+export const SET_IS_ERROR = 'SET_IS_ERROR';
+export const SET_IS_ERROR_TRIGGER = 'SET_IS_ERROR_TRIGGER';
