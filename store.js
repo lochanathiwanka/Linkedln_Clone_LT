62f3b8f24bdf6ecb0c67281bdc5191c4e67bde3e
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-11-27 20:23:34 +0530

    added signUpReducer

--- store.js
+++ store.js
@@ -3,10 +3,13 @@ import createSagaMiddleware from "redux-saga";
 import {spawn} from "@redux-saga/core/effects";
 import signInReducer from "./screens/root-stack-screen/root-stacks/sign-in-screen/redux/signInReducer";
 import signInSaga from "./screens/root-stack-screen/root-stacks/sign-in-screen/redux/signInSaga";
+import signUpReducer from "./screens/root-stack-screen/root-stacks/sign-up-screen/redux/signUpReducer";
+import signUpSaga from "./screens/root-stack-screen/root-stacks/sign-up-screen/redux/signUpSaga";
 
 // root reducer
 const rootReducer = combineReducers({
     signInReducer,
+    signUpReducer
 });
 
 // saga middleware
@@ -18,6 +21,7 @@ const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 // root saga
 function* rootSaga() {
     yield spawn(signInSaga);
+    yield spawn(signUpSaga);
 }
 
 // run saga middleware
