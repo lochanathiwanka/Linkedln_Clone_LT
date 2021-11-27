16531266f004d8202457be834fdb38ada093f1c0
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-11-27 20:17:32 +0530

    updated

--- App.js
+++ App.js
@@ -7,30 +7,17 @@
  */
 import 'react-native-gesture-handler';
 import type {Node} from 'react';
-import React, {useEffect, useState} from 'react';
-import {NavigationContainer} from "@react-navigation/native";
-import RootStackScreen from "./screens/root-stack-screen/pages/RootStackScreen";
-import LoaderScreen from "./screens/root-stack-screen/root-stacks/loader-screen/pages/LoaderScreen";
+import React from 'react';
+import {Provider} from "react-redux";
+import store from "./store";
+import Main from "./screens/Main";
 
 const App: () => Node = () => {
 
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setIsLoading(false);
-        }, 2500);
-        return () => {
-            clearTimeout(timeout);
-        }
-    }, []);
-
     return (
-        isLoading ? <LoaderScreen/> : (
-            <NavigationContainer>
-                <RootStackScreen/>
-            </NavigationContainer>
-        )
+        <Provider store={store}>
+            <Main/>
+        </Provider>
     );
 };
 
