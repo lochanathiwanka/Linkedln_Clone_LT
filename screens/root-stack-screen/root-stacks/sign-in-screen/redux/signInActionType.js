bfa961f661219fc678a307ca282504434815509e
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-12-01 13:36:42 +0530

    inserted addUser & removeUser action types

--- screens/root-stack-screen/root-stacks/sign-in-screen/redux/signInActionType.js
+++ screens/root-stack-screen/root-stacks/sign-in-screen/redux/signInActionType.js
@@ -4,3 +4,7 @@ export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
 export const SIGN_IN_FAILURE = 'SIGN_IN_FAILURE';
 export const SET_IS_ERROR = 'SET_IS_ERROR';
 export const SET_IS_ERROR_TRIGGER = 'SET_IS_ERROR_TRIGGER';
+export const ADD_USER = 'ADD_USER';
+export const ADD_USER_TRIGGER = 'ADD_USER_TRIGGER';
+export const REMOVE_USER = 'REMOVE_USER';
+export const REMOVE_USER_TRIGGER = 'REMOVE_USER_TRIGGER';
