d7fc83847c5667ea010b34e0812baf44435618f1
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-12-02 13:42:38 +0530

    created signUpActionType

---
+++ screens/root-stack-screen/root-stacks/sign-up-screen/redux/signUpActionType.js
@@ -0,0 +1,4 @@
+export const SIGN_UP = 'SIGN_UP';
+export const SIGN_UP_WITH_GOOGLE = 'SIGN_UP_WITH_GOOGLE';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
